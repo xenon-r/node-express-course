2f56e9075359933cc61f927738c45e24a05cec0f
xenon <cj.rdwn@gmail.com>
2021-11-06 19:12:24 +0600

    basic express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,19 @@
-console.log('Express Tutorial')
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Home Page");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Resource Not found<h1/>");
+});
+
+app.listen(5000, () => {
+  console.log(`SERVER IS LISTENING ON PORT ${5000}....`);
+});
