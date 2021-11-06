0eb1b96cf6e8633e7cdc5a4da1a6ef7ced6be905
xenon <cj.rdwn@gmail.com>
2021-11-06 19:33:05 +0600

    express api basic

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,17 +1,9 @@
 const express = require("express");
-
+const { products } = require("./data");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("Home Page");
-});
-
-app.get("/about", (req, res) => {
-  res.status(200).send("About Page");
-});
-
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>Resource Not found<h1/>");
+  res.json(products);
 });
 
 app.listen(5000, () => {
